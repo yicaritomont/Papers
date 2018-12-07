c91ae4560e5ce7e2024aa51f4eae1154e4ccf3fe
hernan.ramirez <hernan.ramirez@turrisystem.com>
2018-12-07 09:03:31 -0500

    organizacion de imagenes al generar pdf

--- app/Http/Controllers/FormatController.php
+++ app/Http/Controllers/FormatController.php
@@ -382,11 +382,19 @@ class FormatController extends Controller
     public function downloadPDF($id)
     {
       $format = Format::find($id);
-      $estilos = Estilo::find(1);
+      $estilos = Estilo::where('name','=','estilo_pdf')->first();
+      $pagination = Estilo::where('name','=','paginate_pdf')->first();
       $eliminar = array('<input style="width:100%" type="text" disabled="">','<input type="text" disabled="">',
         '<textarea disabled="">','<textarea cols="80" rows="10" disabled="">','</textarea>');
-      $format_replace = str_replace($eliminar,'',$format->format);
-      $config_format = $estilos->estilos.$format_replace;
+      $format_pdf = str_replace($eliminar,'',$format->format);
+      $supports = File::where('format_id','=',$format->id)->get();
+      $file_pdf = '';
+      foreach( $supports AS $key => $item )
+      {
+          $file_pdf .= '<div class="contenedor_image"><img class="image" src="'.public_path().'/'.$item->nombre_url.'"/></di>';
+      }
+      //echo "<pre>";print_r($file_pdf);echo "</pre>";exit();
+      $config_format = $estilos->estilos.$format_pdf.$file_pdf.$pagination->estilos;
       $pdf = \App::make('dompdf.wrapper');
       $pdf->getDomPDF()->set_option("enable_php", true);
       $pdf->loadHTML($config_format);
